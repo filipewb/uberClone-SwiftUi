5852e8cf062d8ee65dc3647723ddc9e430a24229
github-personal <filipe.boeck@hotmail.com>
2023-03-25 17:07:59 -0300

    Initial Commit

---
+++ UberCloneSwiftUi/UberCloneSwiftUiApp.swift
@@ -0,0 +1,17 @@
+//
+//  UberCloneSwiftUiApp.swift
+//  UberCloneSwiftUi
+//
+//  Created by wolf on 25/03/23.
+//
+
+import SwiftUI
+
+@main
+struct UberCloneSwiftUiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
