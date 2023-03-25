5c0244ecede5e1dad8c436dfd265c7607f4f6118
github-personal <filipe.boeck@hotmail.com>
2023-03-25 19:10:03 -0300

    feat: create location search ui

---
+++ UberCloneSwiftUi/Core/Home/Views/HomeView.swift
@@ -0,0 +1,41 @@
+//
+//  HomeView.swift
+//  UberCloneSwiftUi
+//
+//  Created by wolf on 25/03/23.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+  @State private var showLocationSearchView = false
+  
+  var body: some View {
+    ZStack(alignment: .top) {
+      UberMapViewRepresentable()
+        .ignoresSafeArea()
+      
+      if showLocationSearchView {
+        LocationSearchView()
+      } else {
+        LocationSearchActivationView()
+          .padding(.top, 72)
+          .onTapGesture {
+            withAnimation(.spring()) {
+              showLocationSearchView.toggle()
+            }
+          }
+      }
+      
+      MapViewActionButton(showLocationSearchView: $showLocationSearchView)
+        .padding(.leading)
+        .padding(.top, 4)
+    }
+  }
+}
+
+struct HomeView_Previews: PreviewProvider {
+  static var previews: some View {
+    HomeView()
+  }
+}
---
+++ UberCloneSwiftUi/Core/LocationSearch/Views/LocationSearchView.swift
@@ -0,0 +1,67 @@
+//
+//  LocationSearchView.swift
+//  UberCloneSwiftUi
+//
+//  Created by wolf on 25/03/23.
+//
+
+import SwiftUI
+
+struct LocationSearchView: View {
+  @State private var startLocationText = ""
+  @State private var destinationLocationText = ""
+  
+  var body: some View {
+    VStack {
+      // header view
+      HStack {
+        VStack {
+          Circle()
+            .fill(Color(.systemGray3))
+            .frame(width: 6, height: 6)
+          
+          Rectangle()
+            .fill(Color(.systemGray3))
+            .frame(width: 1, height: 24)
+          
+          Rectangle()
+            .fill(.black)
+            .frame(width: 6, height: 6)
+        } // : VSTACK
+        
+        VStack {
+          TextField("Current location", text: $startLocationText)
+            .frame(height: 32)
+            .background(Color(.systemGroupedBackground))
+            .padding(.trailing)
+          
+          TextField("Where to?", text: $destinationLocationText)
+            .frame(height: 32)
+            .background(Color(.systemGray4))
+            .padding(.trailing)
+        } // : VSTACK
+      } // : HSTACK
+      .padding(.horizontal)
+      .padding(.top, 64)
+      
+      Divider()
+        .padding(.vertical)
+      
+      // list view
+      ScrollView {
+        VStack(alignment: .leading) {
+          ForEach(0 ..< 20, id: \.self) { _ in
+            LocationSearchResultCell()
+          }
+        }
+      }
+    }
+    .background(.white)
+  }
+}
+
+struct LocationSearchView_Previews: PreviewProvider {
+  static var previews: some View {
+    LocationSearchView()
+  }
+}
